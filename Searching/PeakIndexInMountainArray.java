1ad04da49084a611d7f220212e35e84ba9b846f5
Prerit Shrivastava <preritshrivastava0712@gmail.com>
2026-02-25 00:24:19 +0530

    Another similar quetion

--- Searching/PeakIndexInMountainArray.java
+++ Searching/PeakIndexInMountainArray.java
@@ -8,11 +8,18 @@ public class PeakIndexInMountainArray {
         while(start < end){
             int mid = start + (end - start) / 2;
             if(arr[mid]  > arr[mid + 1]) {
+                //you are in dec  part of array
+                //this may be the answer, but look at left
+                //this is why end != mid - 1
                 end = mid;
             } else {
-                start = mid + 1;
+                //you are in ascendeing part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
             }
         }
-        return start;
+        //in the end , start == end and pointing to the largest number because of the 2 checks above
+        //start and end are always trying to find max element in the above 2 checks
+        //hence, when they are pointint to just one elemnt,
+        return start; // or return end both are equal
     }
 }

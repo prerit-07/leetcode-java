277908364fe98c75c7c2d76971ed45bf94de5060
Prerit Shrivastava <preritshrivastava0712@gmail.com>
2026-02-25 00:03:32 +0530

    Binary search problems

---
+++ Searching/Firstandlastposition.java
@@ -0,0 +1,45 @@
+package LEETCODE.Searching;
+
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+//34
+public class Firstandlastposition {
+    public int[] searchRange(int[] nums, int target){
+
+        int[] ans = {-1 , -1};
+
+        //check for first occurence of target first
+        int start = search(nums , target , true);
+        int end = search(nums , target , false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    //this fumction just return the index value of target
+    int search(int[] nums, int target , boolean findStartindex){
+        int ans  = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else{
+                //potential ans found
+                ans  = mid;
+                if(findStartindex){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
